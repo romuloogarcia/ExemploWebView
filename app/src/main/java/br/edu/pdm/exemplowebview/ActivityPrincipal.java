2352511365114735b5469c37d9748d95086e1fc9
Rômulo Augusto Oestreich Maciel Garcia <romulo.smo@gmail.com>
2016-11-28 09:07:41 -0200

    Initial commit

---
+++ app/src/main/java/br/edu/pdm/exemplowebview/ActivityPrincipal.java
@@ -0,0 +1,13 @@
+package br.edu.pdm.exemplowebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
